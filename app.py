7ba7c87a21c2865600097354ff1ed8b9419fa9a6
Alexander Minaku <al.motologa@gmail.com>
2023-03-26 22:30:12 +0300

    HW

---
+++ app.py
@@ -0,0 +1,13 @@
+def wrap_brackets(text):
+    return "(" + text + ")"
+
+
+def wrap_brackets_sq(text):
+    return "[" * 3 + text + "]" * 3
+
+
+def wrap_brackets_tr(text):
+    return "<" * 3 + text + ">" * 3
+
+
+print(wrap_brackets_tr(wrap_brackets_sq(wrap_brackets("12345"))))
